57dc3ca7e18bf19fbb3709fae0cff83f76fd9694
Junwei Pei <junwei.pei@ubisoft.com>
2020-09-04 12:22:55 +0800

    assert insert

--- myRenderer3/bufferOps.cpp
+++ myRenderer3/bufferOps.cpp
@@ -2,15 +2,19 @@
 #include "logger.h"
 #include "bufferOps.h"
 
+//#define NDEBUG
+#include <cassert>
+
 void tokeniseBuffer(char * buffer)
 {
-    logmsg("buffer:\n%s\n", buffer);
+    assert(buffer != NULL);
+    //logmsg("buffer:\n%s\n", buffer);
     char *nextToken;
     const char * delim = " \r\n";
     char * token = strtok_s(buffer, delim, &nextToken);
     while (token != NULL)
     {
-        //logmsg("token = %s\n", token);
+        logmsg("token = %s\n", token);
         token = strtok_s(NULL, delim, &nextToken);
     }
 }
