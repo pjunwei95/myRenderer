bd448d5d7cf71359544a598125bb3732476311ac
Junwei Pei <junwei.pei@ubisoft.com>
2020-09-07 16:38:21 +0800

    rename profile tracking

--- myRenderer3/profiler.cpp
+++ myRenderer3/profiler.cpp
@@ -16,18 +16,14 @@ void testProfiling()
     }
     int sum = 0;
     PROFILE_BEGIN(test1);
-
     for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
     {
         PROFILE_BEGIN(test2);
-
         for (int j = 0; j < sizeof(arr)/sizeof(arr[0]); j++)
         {
             sum += arr[i][j];
         }
         PROFILE_END(test2);
-
-
     }
     PROFILE_END(test1);
     //logmsg("sum = %d\n", sum);
--- myRenderer3/profiler.h
+++ myRenderer3/profiler.h
@@ -4,12 +4,6 @@
 
 //#define PROFILE_FUNCTION()
 //#define PROFILE_SCOPED(name)
-//
-//
-////each profiling blocks will store the CPU time in milli - seconds
-////then you should have an option to dump out the profiling results to a text file
-////for the past ~50 frames or so
-
 
 #define PROFILE_BEGIN(name) \
                                 Timer name##StartTime;\
@@ -23,10 +17,9 @@
                         logmsg("Time elapsed for %s = %.2f ms\n", #name, getTimerElapsedMs(&name##StopTime));\
 
 
-//#define PROFILE_FUNCTION()
 #define PROFILE_INIT() initialiseTimer(); setIsTrackProfile(false); setCount(50); \
 
-#define TEST() setIsTrackProfile(true); setCount(0);\
+#define PROFILE_TRACK() setIsTrackProfile(true); setCount(0);\
 
 void testProfiling();
 
