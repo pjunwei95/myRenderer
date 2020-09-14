a1ba625978811625bdab5b287534c52f031336d3
Junwei Pei <junwei.pei@ubisoft.com>
2020-09-14 17:04:54 +0800

    added size and remove arr assert

--- myRenderer3/array.cpp
+++ myRenderer3/array.cpp
@@ -9,7 +9,7 @@ struct Array {
     void* m_Data; // pointer to array
     int m_Size; // number of elements
     int m_Capacity; // available memory size
-    unsigned int m_typeSize; // size of element type
+    unsigned int m_TypeSize; // size of element type
 };
 
 Array createNewArray(unsigned int sizeElem) {
@@ -17,16 +17,18 @@ Array createNewArray(unsigned int sizeElem) {
     a.m_Data = nullptr;
     a.m_Size = 0;
     a.m_Capacity = 0;
-    a.m_typeSize = sizeElem;
+    a.m_TypeSize = sizeElem;
     return a;
 }
 
 bool a_empty(const Array* const dstArr)
 {
-    if (dstArr->m_Size > 0)
-        return false;
-    else
-        return true;
+    /*return (dstArr->m_Size > 0) ? false : true;*/
+}
+
+int a_size(const Array* const arr)
+{
+    return arr->m_Size;
 }
 
 void array_push(Array* const dstArr, const void* srcData, int sizeElem)
@@ -34,38 +36,41 @@ void array_push(Array* const dstArr, const void* srcData, int sizeElem)
     assert(dstArr);
     assert(srcData);
     assert(sizeElem > 0);
+    void* ptr;
     if (!dstArr->m_Data)
     {
-        dstArr->m_Data = malloc(sizeElem);
+        /*ptr = malloc(sizeElem);
+        assert(ptr);
+        dstArr->m_Data = ptr;*/
         dstArr->m_Capacity++;
     }
 
     if (dstArr->m_Size == dstArr->m_Capacity)
     {
         dstArr->m_Capacity *= 2;
-        dstArr->m_Data = realloc(dstArr->m_Data, dstArr->m_Capacity * sizeElem);
+       /* ptr = realloc(dstArr->m_Data, dstArr->m_Capacity * sizeElem);
+        assert(ptr);
+        dstArr->m_Data = ptr;*/
     }
-
-    void* ptr = (unsigned char *)dstArr->m_Data + sizeElem * dstArr->m_Size;
+    ptr = (unsigned char *)dstArr->m_Data + sizeElem * dstArr->m_Size;
     memcpy(ptr, srcData, sizeElem);
     dstArr->m_Size++;
 }
 
-void* a_front(const Array* const dstArr)
+void* a_front(const Array* const arr)
 {
-    assert(!a_empty(dstArr));
-    assert(dstArr);
-    assert(dstArr->m_Data);
-    return (unsigned char*)dstArr->m_Data;
+    assert(!a_empty(arr));
+    assert(arr);
+    assert(arr->m_Data);
+    return (unsigned char*)arr->m_Data;
 }
 
-void* a_back(const Array* const dstArr)
+void* a_back(const Array* const arr)
 {
-    assert(!a_empty(dstArr));
-    assert(dstArr);
-    assert(dstArr->m_Data);
-    int lastIdx = dstArr->m_Size - 1;
-    return (unsigned char*)dstArr->m_Data + lastIdx * dstArr->m_typeSize;
+    assert(!a_empty(arr));
+    assert(arr->m_Data);
+    int lastIdx = arr->m_Size - 1;
+    return (unsigned char*)arr->m_Data + lastIdx * arr->m_TypeSize;
 }
 
 void a_free(Array* const dstArr)
@@ -74,24 +79,25 @@ void a_free(Array* const dstArr)
     free(dstArr->m_Data);
 }
 
-void* a_at(const Array* const dstArr, int index)
+void* a_at(const Array* const arr, int index)
 {
+    assert(!a_empty(arr));
+    assert(arr->m_Data);
     assert(index >= 0);
-    assert(index < dstArr->m_Size);
-    return (unsigned char*)dstArr->m_Data + index * dstArr->m_typeSize;
+    assert(index < arr->m_Size);
+    return (unsigned char*)arr->m_Data + index * arr->m_TypeSize;
 }
 
 void testArray()
 {
-    //testing purposes
-    //std::vector<char> test;
-    //test.empty();
-
     Array a = createNewArray(sizeof(int));
     int num = 1;
+    
+    //push 10 elements and print
     for (int i = 0; i < 10; ++i)
     {
         array_push(&a, &num, sizeof(int));
+        num++;
     }
     for (int i = 0; i < 10; ++i)
     {
@@ -99,10 +105,14 @@ void testArray()
         printf("array [%d] = %d\n", i, numAtIdx);
     }
     
+    //print first
     int getFirst = *((int*) a_front(&a));
-
     printf("first = %d\n", getFirst);
+
+    //print last
     int getLast = *((int*)a_back(&a));
     printf("last = %d\n", getLast);
+
+    //free after usage
     a_free(&a);
 }
