126d61a31e3643089ca84d4851122a98ef688021
Junwei Pei <junwei.pei@ubisoft.com>
2020-09-18 16:49:11 +0800

    fixed pushing bug where front and back gets wrongly updated

--- myRenderer3/circularBuffer.cpp
+++ myRenderer3/circularBuffer.cpp
@@ -17,6 +17,7 @@ void c_init()
 
 void c_read(const Array* const circBuf)
 {
+    
     int tempFront = front;
     printf("Reading..\n");
     for (int i = 0; i < BUFFER_LENGTH; ++i)
@@ -38,15 +39,15 @@ void c_push(Array* const array, const void* srcData)
     void* element = a_at(array, back);
     memcpy(element, srcData, array->m_TypeSize);
 
-    // Advances back
-    back = (back + 1) % BUFFER_LENGTH;
-
     // When you 'pop', you simply move the 'front' cursor forward by one
     // Advances front to be always in front of back
     if (front == back)
     {
         front = (front + 1) % BUFFER_LENGTH;
     }
+
+    // Advances back
+    back = (back + 1) % BUFFER_LENGTH;
 }
 
 void testCircularBuffer()
@@ -55,22 +56,15 @@ void testCircularBuffer()
     int empty = 0;
     //init fixed empty array
     Array intCircBuf = a_create_new_filled(BUFFER_LENGTH, &empty, sizeof(int));
-    //Array doubleCircBuf = a_create_new_filled(BUFFER_LENGTH, &empty, sizeof(double));
 
     //push 10 numbers
     for (int i = 0; i < 10; ++i)
     {
         c_push(&intCircBuf, &i);
-        //c_push(&doubleCircBuf, &i);
     }
 
     //print to console
     c_read(&intCircBuf);
 
-    ////test double
-    //void* ptr = (unsigned char*)doubleCircBuf.m_Data + (1 * doubleCircBuf.m_TypeSize);
-    //printf("double[1] = %f\n", *((double*)ptr));
-    
     a_free(&intCircBuf);
-    //a_free(&doubleCircBuf);
 }
