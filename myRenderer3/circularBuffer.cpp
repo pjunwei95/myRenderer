2430fa77fb7cd645997b0bd114e7b9797bf77e0a
Junwei Pei <junwei.pei@ubisoft.com>
2020-09-18 16:25:20 +0800

    tested for double and float, working, requires for loop type to change

--- myRenderer3/circularBuffer.cpp
+++ myRenderer3/circularBuffer.cpp
@@ -7,28 +7,6 @@
 
 int front, back;
 
-void c_push(Array* const array, const void* srcData)
-{
-    assert(array->m_Data);
-    assert(srcData);
-
-
-    //'push' a value directly into the 'back' slot
-    //array[back] = value;
-    void* element = a_at(array, back);
-    memcpy(element, srcData, array->m_TypeSize);
-
-    // Advances back
-    back = (back + 1) % BUFFER_LENGTH;
-
-    // When you 'pop', you simply move the 'front' cursor forward by one
-    // Advances front to be always in front of back
-    if (front == back)
-    {
-        front = (front + 1) % BUFFER_LENGTH;
-    }
-}
-
 //A circular buffer is simply an array but with 2 cursors, front & back
 //the cursors are simply index values
 void c_init()
@@ -50,20 +28,49 @@ void c_read(const Array* const circBuf)
     printf("\n");
 }
 
+void c_push(Array* const array, const void* srcData)
+{
+    assert(array->m_Data);
+    assert(srcData);
+
+    //'push' a value directly into the 'back' slot
+    //array[back] = value;
+    void* element = a_at(array, back);
+    memcpy(element, srcData, array->m_TypeSize);
+
+    // Advances back
+    back = (back + 1) % BUFFER_LENGTH;
+
+    // When you 'pop', you simply move the 'front' cursor forward by one
+    // Advances front to be always in front of back
+    if (front == back)
+    {
+        front = (front + 1) % BUFFER_LENGTH;
+    }
+}
+
 void testCircularBuffer()
 {
     c_init();
     int empty = 0;
     //init fixed empty array
-    Array circBuf = a_create_new_filled(BUFFER_LENGTH, &empty, sizeof(int));
+    Array intCircBuf = a_create_new_filled(BUFFER_LENGTH, &empty, sizeof(int));
+    //Array doubleCircBuf = a_create_new_filled(BUFFER_LENGTH, &empty, sizeof(double));
 
     //push 10 numbers
     for (int i = 0; i < 10; ++i)
     {
-        c_push(&circBuf, &i);
+        c_push(&intCircBuf, &i);
+        //c_push(&doubleCircBuf, &i);
     }
 
     //print to console
-    c_read(&circBuf);
-    a_free(&circBuf);
+    c_read(&intCircBuf);
+
+    ////test double
+    //void* ptr = (unsigned char*)doubleCircBuf.m_Data + (1 * doubleCircBuf.m_TypeSize);
+    //printf("double[1] = %f\n", *((double*)ptr));
+    
+    a_free(&intCircBuf);
+    //a_free(&doubleCircBuf);
 }
