bac696547d974b9631045f9431f2c6857e7435ed
Junwei Pei <junwei.pei@ubisoft.com>
2020-09-24 10:01:12 +0800

    renamed functions cb

--- myRenderer3/circularBuffer.cpp
+++ myRenderer3/circularBuffer.cpp
@@ -8,7 +8,7 @@ void printCircBuf(const CircularBuffer* const cb);
 
 // A circular buffer is simply an array but with 2 cursors, front & back
 // the cursors are simply index values
-CircularBuffer cb_create_new(const void* const elemVal, unsigned int sizeElem)
+CircularBuffer createNewCircBuf(const void* const elemVal, unsigned int sizeElem)
 {
     CircularBuffer cb;
     cb.m_Array = a_create_new_filled(BUFFER_LENGTH, elemVal, sizeElem);
@@ -17,7 +17,7 @@ CircularBuffer cb_create_new(const void* const elemVal, unsigned int sizeElem)
     return cb;
 }
 
-void cb_pop(CircularBuffer* const cb)
+void popCircBuf(CircularBuffer* const cb)
 {
     assert(cb->m_Array.m_Data);
     if (cb->m_Front == cb->m_Back)
@@ -27,7 +27,7 @@ void cb_pop(CircularBuffer* const cb)
 }
 
 //Assuming fixed size buffer
-void cb_push(CircularBuffer* const cb, const void* srcData)
+void pushCircBuf(CircularBuffer* const cb, const void* srcData)
 {
     assert(cb->m_Array.m_Data);
     assert(srcData);
@@ -40,13 +40,13 @@ void cb_push(CircularBuffer* const cb, const void* srcData)
 
     // When you 'pop', you simply move the 'front' cursor forward by one
     // Advances front to be always in front of back
-    cb_pop(cb);
+    popCircBuf(cb);
 
     // Advances back
     cb->m_Back = (cb->m_Back + 1) % BUFFER_LENGTH;
 }
 
-void cb_free(CircularBuffer* const cb)
+void freeCircBuf(CircularBuffer* const cb)
 {
     assert(cb);
     a_free(&cb->m_Array);
@@ -55,25 +55,25 @@ void cb_free(CircularBuffer* const cb)
 void testCircularBuffer()
 {
     int empty = 0;
-    CircularBuffer intCircBuf = cb_create_new(&empty, sizeof(int));
+    CircularBuffer intCircBuf = createNewCircBuf(&empty, sizeof(int));
 
     //push 10 numbers
     for (int i = 0; i < 15; ++i)
     {
-        cb_push(&intCircBuf, &i);
+        pushCircBuf(&intCircBuf, &i);
     }
 
     //print to console
     printCircBuf(&intCircBuf);
 
     printf("Popping and pushing 99...\n");
-    cb_pop(&intCircBuf);
+    popCircBuf(&intCircBuf);
     int rand = 99;
-    cb_push(&intCircBuf, &rand);
+    pushCircBuf(&intCircBuf, &rand);
 
     printCircBuf(&intCircBuf);
 
-    cb_free(&intCircBuf);
+    freeCircBuf(&intCircBuf);
 }
 
 void printCircBuf(const CircularBuffer* const cb)
