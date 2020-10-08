27bec23653bcc1541482164e26f2066bfc131df4
Junwei Pei <junwei.pei@ubisoft.com>
2020-10-08 16:03:09 +0800

    spacing

--- myRenderer3/runMainLoop.cpp
+++ myRenderer3/runMainLoop.cpp
@@ -13,7 +13,6 @@ bool isTestProfile = true;
 
 void runMainLoop()
 {
-
     // Enable run-time memory check for debug builds.
     //#if defined(DEBUG) | defined(_DEBUG)
     //_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
--- myRenderer3/stack.h
+++ myRenderer3/stack.h
@@ -7,12 +7,19 @@ template <typename T>
 class Stack {
 public:
     Stack();
+
     ~Stack();
+
     void push(T* srcData);
+
     void pop();
+
     T* peek();
+
     T* at(uint32_t index);
+
     uint32_t size();
+
 private:
     Array<T>* m_Stack;
 };
