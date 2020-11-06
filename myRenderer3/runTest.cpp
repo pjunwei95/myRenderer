7c43f7911212b854843225bcf35363b25186e3e9
Junwei Pei <junwei.pei@ubisoft.com>
2020-11-06 15:30:54 +0800

    added runTest.cpp from .h

---
+++ myRenderer3/runTest.cpp
@@ -0,0 +1,15 @@
+#include <crtdbg.h>
+#include "array_unitTest.h"
+#include "stack_unitTest.h"
+#include "profileManager_unitTest.h"
+#include "circularBuffer_unitTest.h"
+
+void runTest()
+{
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    // TESTS
+    //testArray();
+    //testCircularBuffer();
+    testProfileManager();
+}
