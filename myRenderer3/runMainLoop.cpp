972ce3213a51ba5bdf56256973ccda3a8c7f5299
Junwei Pei <junwei.pei@ubisoft.com>
2020-11-06 13:49:30 +0800

    removed headers

--- myRenderer3/runMainLoop.cpp
+++ myRenderer3/runMainLoop.cpp
@@ -1,15 +1,12 @@
+#include <crtdbg.h>
 #include "engine.h"
 #include "getKeyInput.h"
 #include "frameRateController.h"
 #include "windowHandler.h"
-#include <crtdbg.h>
-//#include "profiler.h"
-#include "test.h"
-#include "stopwatch.h"
 
 void runMainLoop()
 {
-    //_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 
     FrameRateController frc;
 
