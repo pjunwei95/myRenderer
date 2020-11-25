43bafbef037b489c6fcbc4355d922f0aed73c6bb
Junwei Pei <junwei.pei@ubisoft.com>
2020-11-25 16:42:47 +0800

    added stopwatch

---
+++ myRenderer3/stopwatch.cpp
@@ -0,0 +1,42 @@
+#include "stopwatch.h"
+
+Stopwatch::Stopwatch()
+    : m_isStopped{ false }
+{
+    start();
+}
+
+Stopwatch::~Stopwatch()
+{
+    if (!m_isStopped)
+        stop(); //stop timer
+}
+
+//This function will stop the time and convert the stoptime to a readable time duration difference
+//in microseconds
+void Stopwatch::stop()
+{
+    updateTimeStamp(m_StopTime);
+    m_StopTime.QuadPart = (m_StopTime.QuadPart - m_StartTime.QuadPart); //the units here are in seconds
+    m_StopTime.QuadPart *= 1000000; //convert seconds to microseconds
+    m_StopTime.QuadPart /= getSystemFrequency().QuadPart; //machine independence
+    m_isStopped = true;
+}
+
+float Stopwatch::getDurationMs()
+{
+    stop();
+    return (float)m_StopTime.QuadPart / 1000;
+}
+
+float Stopwatch::getDurationSeconds()
+{
+    stop();
+    return (float)m_StopTime.QuadPart / 1000000;
+}
+
+Timer Stopwatch::getDurationUs()
+{
+    stop();
+    return m_StopTime;
+}
--- myRenderer3/stopwatch.h
+++ myRenderer3/stopwatch.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "engine.h"
-#include <assert.h>
 
 class Stopwatch 
 {
@@ -8,54 +7,20 @@ public:
     typedef LARGE_INTEGER Timer;
     typedef LARGE_INTEGER& TimerHandle;
 
-    Stopwatch() : m_isStopped{ false }
-    {
-        start();
-    }
-    ~Stopwatch() 
-    {
-        if (!m_isStopped)
-            stop(); //stop timer
-    }
+    Stopwatch();
+    ~Stopwatch();
 
-    void start()
-    {
-        updateTimeStamp(m_StartTime); //start timer
-    }
+    inline void updateTimeStamp(TimerHandle timer) { QueryPerformanceCounter(&timer); }
+
+    //start timer
+    inline void start() { updateTimeStamp(m_StartTime); }
 
     //This function will stop the time and convert the stoptime to a readable time duration difference
     //in microseconds
-    void stop()
-    {
-        updateTimeStamp(m_StopTime);
-        m_StopTime.QuadPart = (m_StopTime.QuadPart - m_StartTime.QuadPart); //the units here are in seconds
-        m_StopTime.QuadPart *= 1000000; //convert seconds to microseconds
-        m_StopTime.QuadPart /= getSystemFrequency().QuadPart; //machine independence
-        m_isStopped = true;
-    }
-
-    void updateTimeStamp(TimerHandle timer)
-    {
-        QueryPerformanceCounter(&timer);
-    }
-
-    float getDurationMs()
-    {
-        stop();
-        return (float)m_StopTime.QuadPart / 1000;
-    }
-
-    float getDurationSeconds()
-    {
-        stop();
-        return (float)m_StopTime.QuadPart / 1000000;
-    }
-
-    Timer getDurationUs() 
-    { 
-        stop();
-        return m_StopTime;
-    }
+    void stop();
+    float getDurationMs();
+    float getDurationSeconds();
+    Timer getDurationUs();
 
 private:
     bool m_isStopped;
